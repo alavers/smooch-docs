f876df127e8b1ddaae8b22e6adc13286ae0e6d52
Andrew Lavers <andrew@smooch.io>
2016-08-31 16:45:38 -0400

    Use temporary docs location

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,55 @@
 /* global require, module, process: false */
 
 module.exports = function(grunt) {
-'use strict';
+    'use strict';
 
-var githubPagesDir = 'gh_pages';
+    var githubPagesDir = 'gh_pages';
 
-require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-grunt.initConfig({
-    exec: {
-        buildSlateDocs: {
-            cmd: function() {
-                return [
-                    'bundle install',
-                    'rm -rf build',
-                    'bundle exec middleman build --clean'
-                ].join(' && ');
-            }
-        },
-        checkoutDocs: {
-            cmd: function() {
-                return [
-                    'rm -rf ' + githubPagesDir,
-                    'git clone git@github.com:supportkit/supportkit-docs.git --branch gh-pages --single-branch ' + githubPagesDir,
-                    'cp -R build/* ' + githubPagesDir
-                ].join(' && ');
-            }
-        },
-        server: {
-            cmd: function() {
-                return 'bundle exec middleman server';
-            }
-        }
-    },
-    githubPages: {
-        docs: {
-            options: {
-                commitMessage: 'Update docs'
+    grunt.initConfig({
+        exec: {
+            buildSlateDocs: {
+                cmd: function() {
+                    return [
+                        'bundle install',
+                        'rm -rf build',
+                        'bundle exec middleman build --clean'
+                    ].join(' && ');
+                }
             },
-            src: githubPagesDir
+            checkoutDocs: {
+                cmd: function() {
+                    return [
+                        'rm -rf ' + githubPagesDir,
+                        'git clone git@github.com:alavers/smooch-docs.git --branch gh-pages --single-branch ' + githubPagesDir,
+                        'cp -R build/* ' + githubPagesDir
+                    ].join(' && ');
+                }
+            },
+            server: {
+                cmd: function() {
+                    return 'bundle exec middleman server';
+                }
+            }
+        },
+        githubPages: {
+            docs: {
+                options: {
+                    commitMessage: 'Update docs'
+                },
+                src: githubPagesDir
+            }
         }
-    }
-});
+    });
 
-grunt.registerTask('logurl', function() {
-    grunt.log.write('http://localhost:4567');
-});
+    grunt.registerTask('logurl', function() {
+        grunt.log.write('http://localhost:4567');
+    });
 
-grunt.registerTask('publish', function() {
-    grunt.task.run('exec:buildSlateDocs', 'exec:checkoutDocs', 'githubPages:docs');
-});
+    grunt.registerTask('publish', function() {
+        grunt.task.run('exec:buildSlateDocs', 'exec:checkoutDocs', 'githubPages:docs');
+    });
 
-grunt.registerTask('default', ['logurl', 'exec:server']);
+    grunt.registerTask('default', ['logurl', 'exec:server']);
 };
